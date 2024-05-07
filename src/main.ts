a97d2f45d0a463f4c5cd53874473c12d3e577008
Warckut <warckut@gmail.com>
2024-05-07 10:56:40 +0500

    validate on

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
 
   const config = new DocumentBuilder().setTitle('Block list').build();
   const document = SwaggerModule.createDocument(app, config);
+  app.useGlobalPipes(new ValidationPipe());
   SwaggerModule.setup('api', app, document);
 
   await app.listen(8080);
